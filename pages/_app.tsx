eeed2593175293160914d6b640aa34de839913f7
rayyo999 <i34050018@gs.ncku.edu.tw>
2022-11-10 23:24:07 +0800

    create all

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { cacheTime: 300000, staleTime: 10000 } },
+  })
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
